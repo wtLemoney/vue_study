a518ae52ce5d16263c9a4ca9d512a03b77c9c8fc
wt <307548594@qq.com>
2018-06-01 13:53:38 +0800

    vue-cli 环境使用

---
+++ vuecli_todolist/src/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+<!-- 只能有一个根元素 -->
+  <div>
+      <div>
+        <input class="item" v-model="inputValue">
+        <button @click="handleSubmit">提交</button>
+    </div>
+  <ul>
+    <todo-item 
+    v-for="(item,index) of list" 
+    :key="index"
+    :content="item"
+    :index="index"
+    @delete="handleDelete"></todo-item>
+  </ul>
+  </div>
+</template>
+
+<script>
+  import TodoItem from './components/TodoItem'
+
+  export default{
+    components:{
+      'todo-item':TodoItem
+    },
+    data: function(){//data是函数
+      return {
+          inputValue: "Hello",
+          list:[]
+      }
+    },
+    methods: {
+      handleSubmit(){
+        this.list.push(this.inputValue);
+        this.inputValue ="";
+    },
+    handleDelete(index){
+      this.list.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
